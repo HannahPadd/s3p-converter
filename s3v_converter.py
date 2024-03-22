26ff6e2c6680fe34fd7bd2406fc22c9ef1291b61
Hannah <57945088+HannahPadd@users.noreply.github.com>
2024-03-22 21:00:02 +0100

    Update s3v_converter.py

--- s3v_converter.py
+++ s3v_converter.py
@@ -1,16 +1,19 @@
 import os
 import ffmpeg
 
+current_dir = os.getcwd()
+
 def search_files():
-    current_dir = os.getcwd()
-    files = [file for file in os.listdir(current_dir) if file.endswith("s3p")]
+    global current_dir
+    files = [file for file in os.listdir(current_dir + "/in") if file.endswith("s3p")]
     return files
 
 def convert_files(files, start = 0, end = 0):
+    global current_dir
     for item in files:
         name = item[:-3] + "asf"
         print(name)
-        with open(item, "rb") as file:
+        with open(current_dir + "/in/" + item, "rb") as file:
             data = file.read()
             try:
                 offset = hex(data.index(b'\x30\x26'))
@@ -18,17 +21,17 @@ def convert_files(files, start = 0, end = 0):
             except ValueError:
                 print("Couldn't find offset")
             converted_file = data[:start] + data[int(offset, 16):]
-        with open(name, 'wb') as file:
+        with open(current_dir +  "/out/" + name, 'wb') as file:
             file.write(converted_file)
 
 def to_wav():
-    current_dir = os.getcwd()
-    files = [file for file in os.listdir(current_dir) if file.endswith("asf")]
+    global current_dir
+    files = [file for file in os.listdir(current_dir + "/out") if file.endswith("asf")]
 
     for file in files:
         filename = file[:-3] + "wav" 
-        ffmpeg.input(file).output(filename). run()
-        os.remove(file)
+        ffmpeg.input(current_dir + "/out/" + file).output(current_dir + "/out/" + filename).run()
+        os.remove(current_dir + "/out/" + file)
 
 def main():
     files = search_files()
@@ -36,7 +39,7 @@ def main():
         print(element)
     convert_files(files)
     print("Do you want to convert to WAV?")
-    answer = input("[Y/n]")
+    answer = input("[Y/n]\n")
     if answer != "n":
         to_wav()
 
